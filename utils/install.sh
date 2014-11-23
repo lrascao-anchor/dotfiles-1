3b5541953e75ddab13b0c0681bfd4245c677450b
Luis Rascao <luis.rascao@miniclip.com>
2014-11-23 17:03:27 +0000

    support oh-my-zsh

--- utils/install.sh
+++ utils/install.sh
@@ -4,3 +4,6 @@ git clone --quiet git@github.com:nojhan/liquidprompt.git utils/liquidprompt
 
 # clone gitcheat
 git clone --quiet git@github.com:0xAX/git-cheat.git utils/git-cheat
+
+# oh my zsh
+git clone --quiet git://github.com/robbyrussell/oh-my-zsh.git utils/oh-my-zsh
--- utils/load.sh
+++ utils/load.sh
@@ -4,3 +4,18 @@ source liquidprompt/liquidprompt
 
 # add git-cheat to path
 export PATH=$PATH:`pwd`/git-cheat
+
+# Path to your oh-my-zsh installation.
+export ZSH=$HOME/utils/oh-my-zsh
+# Set name of the theme to load.
+# Look in ~/.oh-my-zsh/themes/
+# Optionally, if you set this to "random", it'll load a random theme each
+# time that oh-my-zsh is loaded.
+ZSH_THEME="robbyrussell"
+
+# Uncomment the following line to disable bi-weekly auto-update checks.
+DISABLE_AUTO_UPDATE="true"
+plugins=(git osx ruby)
+
+# load up oh my zsh
+source $ZSH/oh-my-zsh.sh
