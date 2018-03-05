40825e5bfecf765ce2345dd636b76cf24a84467f
Luis Rascao <luis.rascao@miniclip.com>
2018-03-05 22:29:26 +0000

    Fix ls colors not showing up

--- utils/load.sh
+++ utils/load.sh
@@ -2,27 +2,6 @@
 # load liquidprompt
 source liquidprompt/liquidprompt
 
-# add git-cheat to path
-export PATH=$PATH:`pwd`/git-cheat
-
-# Path to your oh-my-zsh installation.
-# export ZSH=$HOME/utils/oh-my-zsh
-# # Set name of the theme to load.
-# # Look in ~/.oh-my-zsh/themes/
-# # Optionally, if you set this to "random", it'll load a random theme each
-# # time that oh-my-zsh is loaded.
-# ZSH_THEME="robbyrussell"
-
-# # Uncomment the following line to disable bi-weekly auto-update checks.
-# DISABLE_AUTO_UPDATE="true"
-# plugins=(osx ruby)
-
-# load up oh my zsh
-# source $ZSH/oh-my-zsh.sh
-
-# load up zsh-syntax-highlighting
-# source ~/utils/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-
 # load up zsh-history-substring-search
 source ~/utils/zsh-history-substring-search/zsh-history-substring-search.zsh
 # bind UP and DOWN arrow keys
