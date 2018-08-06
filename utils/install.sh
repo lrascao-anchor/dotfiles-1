9108a4ae93307e03c6cf9a38490d4cf4e8f05d3a
Luis Rascao <luis.rascao@miniclip.com>
2018-08-06 12:57:21 +0100

    Always use https github endpoints

--- utils/install.sh
+++ utils/install.sh
@@ -1,27 +1,27 @@
 
 # clone liquid prompt
-git clone --quiet git@github.com:nojhan/liquidprompt.git utils/liquidprompt
+git clone --quiet https://github.com:nojhan/liquidprompt.git utils/liquidprompt
 
 # clone gitcheat
-git clone --quiet git@github.com:0xAX/git-cheat.git utils/git-cheat
+git clone --quiet https://github.com:0xAX/git-cheat.git utils/git-cheat
 
 # oh my zsh
-git clone --quiet git://github.com/robbyrussell/oh-my-zsh.git utils/oh-my-zsh
+git clone --quiet https://github.com/robbyrussell/oh-my-zsh.git utils/oh-my-zsh
 
 # zsh-syntax-highlighting
-git clone --quiet git@github.com:zsh-users/zsh-syntax-highlighting.git
+git clone --quiet https://github.com:zsh-users/zsh-syntax-highlighting.git
 
 # zsh-history-substring-search
-git clone --quiet git://github.com/zsh-users/zsh-history-substring-search.git
+git clone --quiet https://github.com/zsh-users/zsh-history-substring-search.git
 
 # zsh autosugestions
-git clone --quiet git://github.com/tarruda/zsh-autosuggestions
+git clone --quiet https://github.com/tarruda/zsh-autosuggestions
 
 # Easy building and installing of Erlang/OTP instances
-git clone --quiet git://github.com/yrashk/kerl.git
+git clone --quiet https://github.com/yrashk/kerl.git
 
 # Additional completion definitions for Zsh
-git clone --quiet git://github.com/zsh-users/zsh-completions.git
+git clone --quiet https://github.com/zsh-users/zsh-completions.git
 
 # https://unbalancedparentheses.github.io/gut/
 wget https://github.com/unbalancedparentheses/gut/raw/master/bin/gut
@@ -29,9 +29,9 @@ chmod +x ./gut
 sudo mv ./gut ~/bin
 
 # git flow
-git clone --quiet git://github.com/petervanderdoes/gitflow.git
+git clone --quiet https://github.com/petervanderdoes/gitflow.git
 # git flow completion
-git clone --quiet git://github.com/bobthecow/git-flow-completion
+git clone --quiet https://github.com/bobthecow/git-flow-completion
 
 # zshell vim plugin
 git clone https://github.com/laurenkt/zsh-vimto.git
